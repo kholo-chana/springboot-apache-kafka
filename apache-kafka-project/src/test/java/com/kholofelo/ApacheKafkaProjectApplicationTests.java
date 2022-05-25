5e19bb55d623d45236754721463233cfd7c27b5c
kholo-chana <phaswane8@gmail.com>
2022-05-26 01:14:23 +0200

    Initial Commit

---
+++ apache-kafka-project/src/test/java/com/kholofelo/ApacheKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kholofelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
